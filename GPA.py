da4f6cbe127d97a8ed2a33bf6e6dab771c2ffada
CicesSpert9 <matiaskueh@gmail.com>
2025-09-18 16:19:29 -0700

    Phase 2

--- GPA.py
+++ GPA.py
@@ -5,9 +5,23 @@ class Student:
         self.courses = {}
     def course(self, name, grades, credits):
         self.courses[name] = (credits, grades)
-        name = input(f"What course are you taking?")
+    def calculate_GPA(self):
+        for credits, grades in self.courses.values():
+            total_points += credits * grades
+            total_credits += credits 
+            GPA = total_points/ total_credits
+    def __str__(self):
+         print("Student: {} (ID: {}) GPA: {}".format(str(self.name), str(self.studentid), str(self.calculate_GPA)))
+
+
+
+
+ name = input(f"What course are you taking?")
         credits = input(f"What are your credits in {self.name}?")
         grades = input(f"What is your grade in {self.name}")
+
+
+
 
 
 
