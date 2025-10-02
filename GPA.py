010bc492dc910dbfde78dbf7cb871a62bb9ad3f0
CicesSpert9 <matiaskueh@gmail.com>
2025-10-02 16:17:12 -0700

    Complete

--- GPA.py
+++ GPA.py
@@ -9,16 +9,28 @@ class Student:
         for credits, grades in self.courses.values():
             total_points += credits * grades
             total_credits += credits 
-            GPA = total_points/ total_credits
+        GPA = total_points/ total_credits
+        return round(GPA, 2) 
+    
+    
     def __str__(self):
-         print("Student: {} (ID: {}) GPA: {}".format(str(self.name), str(self.studentid), str(self.calculate_GPA)))
+        
+        return print("Student: {} (ID: {}) GPA: {}".format(str(self.name), str(self.studentid), str(self.calculate_GPA)))
+         
+
+name = input(f"What is your student name?")
+studentid = input(f"What is your student ID?")
+student = Student(name, studentid)
+
+while True:
+    course = input(f"What course are you taking?")
+    credits = input(f"What are your credits in {course}?")
+    grades = input(f"What is your grade in {course}")
+    if course.lower() == "done":
+        
 
 
-
-
- name = input(f"What course are you taking?")
-        credits = input(f"What are your credits in {self.name}?")
-        grades = input(f"What is your grade in {self.name}")
+        
 
 
 
